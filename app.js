0ebb7883296b752623b967ac7b44464536ab550c
austinc <ac6404@gmail.com>
2021-02-27 13:26:03 -0600

    test first successful batch upload

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
-const settings = require("./settings");
+const fetch = require('node-fetch');
 
+const settings = require("./settings");
 const vaccinefinder = require("./scripts/vaccinefinder");
 const mogov = require("./scripts/mogov.js");
 
+const url = process.env.VAXBOT_API_URL
 
 // where we'll store data as it arrives.
 let results = [];
+
 
 //scripts to run
 const scripts = [
@@ -14,10 +17,37 @@ const scripts = [
 	mogov,
 ]
 
+
 function concat_arrays(arr1, arr2) {
 	let temp = arr1.concat(arr2);
 	return temp
 }
+
+
+async function send_data() {								// NO SUPPORT FOR MULTIPLE CHANNELS RN
+	const channel = await fetch(url + "/v1/channels")
+		.then(async res => await res.json())
+		//~ .then(json => console.log(json))
+		.then(json => json.filter(channel => channel.name === settings.channel))
+		.then(channel => channel[0])
+	
+	for(let item of results) {
+		item.channels = [];
+		item.channels.push(channel._id);
+	}
+	
+	console.log(results);
+	
+	await fetch(url + "/v1/locations/batch", {
+		method: 'POST',
+		body: JSON.stringify(results),
+		headers: { 'Content-Type': 'application/json' }
+	})
+		//~ .then(async res => await res.json())
+		.then(json => console.log(json))
+		
+}
+
 
 async function start_crawler() {
 	for(let script of scripts) {
@@ -28,12 +58,11 @@ async function start_crawler() {
 			console.log(`FAILURE. Aborting script... \n ${err}`);
 		}
 	}
-	
-	for(let item of results) {
-		item.channels = [settings.channel];
+	try {
+		send_data()
+	} catch (err) {
+		console.log(`ERROR. Unable to send data: \n ${err}`)
 	}
-	
-	console.log(results);
 }
 
 start_crawler();
